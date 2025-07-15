59ae588278d238220fbaf5ff7b069956548ada2c
paleon08 <paleon08@naver.com>
2025-07-15 14:46:12 +0900

    fb1

--- firebase.js
+++ firebase.js
@@ -1,12 +1,10 @@
-// server/firebase.js
+// firebase.js
 const admin = require('firebase-admin');
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.applicationDefault(), // Render에서는 이거!
+  databaseURL: 'https://threeseasproject-d3721.firebaseio.com' // 정확히 프로젝트 ID 반영
 });
 
 const db = admin.firestore();
-
 module.exports = db;
