d37494be02137f9d53423ba9d2ceb9ddfd71fdef
paleon08 <paleon08@naver.com>
2025-07-15 16:21:10 +0900

    f

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@
 const admin = require('firebase-admin');
 
 // Render에서는 환경변수에 서비스 계정 JSON 전체를 문자열로 저장하고 사용
-const raw = process.env.FIREBASE_KEY;
+const raw = process.env.FIREBASE_CONFIG;
 
 if (!raw) {
   throw new Error("❌ FIREBASE_CONFIG 환경변수가 설정되지 않았습니다.");
