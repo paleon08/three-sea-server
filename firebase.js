a2b5c32b4723794d63ccecf8a4321c94e9cac69e
paleon08 <paleon08@naver.com>
2025-07-15 15:36:11 +0900

    fbkey

--- firebase.js
+++ firebase.js
@@ -2,7 +2,13 @@
 const admin = require('firebase-admin');
 
 // Render에서는 환경변수에 서비스 계정 JSON 전체를 문자열로 저장하고 사용
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);  // 환경변수에서 파싱
+const raw = process.env.FIREBASE_KEY;
+
+if (!raw) {
+  throw new Error("❌ FIREBASE_CONFIG 환경변수가 설정되지 않았습니다.");
+}
+
+const serviceAccount = JSON.parse(raw);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
